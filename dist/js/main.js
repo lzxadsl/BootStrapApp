b9e769f9c6159c28ab43cac8ac22b66ba7a7b5ea
lizx <lizx@ihczd.com>
2015-11-10 18:06:18 +0800

    测试提交
    Signed-off-by: lizx <lizx@ihczd.com>

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,11 +3,6 @@
  * 主模块,存放公共函数
  */
 define(['html5shiv.min','respond.min','jquery.min'],function(){
-  $.extend({
-    add:function(arg){
-      alert(arg);
-    }
-  });
   return {
     test:function(){
       alert('test');
